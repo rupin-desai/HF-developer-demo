15a4f81a42e48c190b51f70f7d2d4f7e62f2d7a2
rupin_desai <rupin360@gmail.com>
2025-06-13 14:21:39 +0530

    Refactor code structure for improved readability and maintainability

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <>
-    <div className="text-8xl">Hello, World 2</div>
+    <div className="text-8xl font-family-satoshi">Hello, World 1 2 3 4 5 </div>
     </>
   );
 }
